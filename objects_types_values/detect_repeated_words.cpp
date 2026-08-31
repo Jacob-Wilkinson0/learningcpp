4d05c463015f9009a1a1582565a4a74f0d9ac45c
Jacob Wilkinson <wilkinsonjacob0321@gmail.com>
2026-08-31 18:53:53 -0400

    Detecting repeating words

---
+++ objects_types_values/detect_repeated_words.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+int main() {
+
+    std::string previous;
+    std::string current;
+
+    while (std::cin >> current) {
+        if (previous == current) {
+            std::cout << "repeated word: " << current << '\n';
+        }
+        previous = current;
+    }
+}
