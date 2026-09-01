5f4a0d3db76ec06533743e886318ded813947e17
Jacob Wilkinson <wilkinsonjacob0321@gmail.com>
2026-08-31 20:05:13 -0400

    Some simple debugging

---
+++ objects_types_values/debugging_pt1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+// Given snippet to debug
+
+// int Main() {
+//
+//     STRING s = "Goodbye, cruel world!";
+//     cOut << S << '\n';
+// }
+
+// Corrected snippet
+
+int main() {
+
+    std::string s = "Goodbye, cruel world!";
+    std::cout << s << '\n';
+
+}
